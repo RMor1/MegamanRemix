b543c68abe03de3c240f66c271c72e263303df78
Chi-ih-co <ra00297308@pucsp.edu.br>
2021-09-19 15:21:55 -0300

    Trigger invisível para cutscene 19-09-2021 15:21

---
+++ MegamanT4UnityProject/Assets/controlePreBoss.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class controlePreBoss : MonoBehaviour
+{
+    public GameObject Player1;
+    public GameObject diretorPreBoss;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayableDirector pd = diretorPreBoss.GetComponent<PlayableDirector>();
+        if (pd != null)
+            pd.Play();
+    }
+}
