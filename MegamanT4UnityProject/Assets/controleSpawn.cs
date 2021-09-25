f2ce54666fdbbbb89fb4011433acb548bb3fcbdc
Chi-ih-co <ra00297308@pucsp.edu.br>
2021-09-25 18:57:38 -0300

    V1 Fase Aquática 25/09/2021 18:57

---
+++ MegamanT4UnityProject/Assets/controleSpawn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controleSpawn : MonoBehaviour
+{
+    bool spawnar = true;
+    [SerializeField]
+    float intervaloSpawn = 2;
+    [SerializeField]
+    GameObject prefabPlatform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        if (spawnar)
+        {
+            Vector3 posSpawn = transform.position;
+            GameObject tempPlataform = Instantiate(prefabPlatform,
+            posSpawn,
+            Quaternion.identity, null);
+            StartCoroutine("frequenciaSpawn");
+        }
+    }
+    IEnumerator frequenciaSpawn()
+    {
+        spawnar = false;
+        yield return new WaitForSeconds(intervaloSpawn);
+        spawnar = true;
+    }
+}
