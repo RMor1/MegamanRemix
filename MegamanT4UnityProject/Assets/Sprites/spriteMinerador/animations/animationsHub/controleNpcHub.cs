87f763ddb24c7348602aad01bfa3c718b80532a9
Chi-ih-co <ra00297308@pucsp.edu.br>
2021-09-26 23:35:08 -0300

    Animação de npcs do hub 26-09-2021 23:25

---
+++ MegamanT4UnityProject/Assets/Sprites/spriteMinerador/animations/animationsHub/controleNpcHub.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controleNpcHub : MonoBehaviour
+{
+    [SerializeField]
+    int controle = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(controle)
+        {
+            case 0:
+                GetComponent<Animator>().SetBool("medo1", true);
+                break;
+            case 1:
+                GetComponent<Animator>().SetBool("medo2", true);
+                break;
+            case 2:
+                GetComponent<Animator>().SetBool("medo3", true);
+                break;
+            case 3:
+                GetComponent<Animator>().SetBool("medo4", true);
+                break;
+        }
+    }
+}
